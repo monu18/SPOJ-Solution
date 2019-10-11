f916c20d7bbb200839fbd6d8ebedc62b0b0e3d63
Monu Kumar <47026116+monu18@users.noreply.github.com>
2019-10-12 00:28:16 +0530

    Add files via upload

---
+++ TEST.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+	// your code goes here
+	while(1)
+	{
+		int a;
+		cin>>a;
+		if(a!=42)
+		{
+			cout<<a<<"\n";
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
+}
